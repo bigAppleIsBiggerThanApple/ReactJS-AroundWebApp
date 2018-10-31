386b2ec9c6a1258d60be2c1683e411c50c8e71f1
bigAppleIsBiggerThanApple <xwan4@hawk.iit.edu>
2018-10-30 22:32:14 -0500

    login page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Register} from "./Register"
+import {Register} from "./Register";
+import {Login} from "./Login";
 
 
 export class Main extends React.Component{
     render(){
         return(
             <div className="main">
-                <Register/>
+                <Login/>
             </div>
         )
     }
