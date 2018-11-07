715a3118c82b2c1ef1dd68360baab39e90a41bcc
bigAppleIsBiggerThanApple <xwan4@hawk.iit.edu>
2018-11-06 23:32:05 -0600

    CreatePostButton down

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,48 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
-import {GEO_OPTIONS} from "../constants"
-import {Spin} from 'antd';
-import {POS_KEY} from "../constants"
-import {API_ROOT} from "../constants"
-import {AUTH_HEADER} from "../constants"
-import {TOKEN_KEY} from "../constants"
-import {Gallery} from "./Gallery"
+import { Tabs, Spin } from 'antd';
+import { GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../constants';
+import { Gallery } from './Gallery';
+import { CreatePostButton } from './CreatePostButton';
 
 const TabPane = Tabs.TabPane;
 
-export class Home extends React.Component{
+export class Home extends React.Component {
     state = {
         isLoadingGeoLocation: false,
         error: '',
         isLoadingPosts: false,
         posts: [],
     }
-    componentDidMount(){
+
+    componentDidMount() {
         if ("geolocation" in navigator) {
-            this.setState({isLoadingGeoLocation: true, error:'',});
+            this.setState({ isLoadingGeoLocation: true, error: '' });
             navigator.geolocation.getCurrentPosition(
-            this.onSuccessLoadGeoLocation,
-            this.onFailedLoadGeoLocation,
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
                 GEO_OPTIONS);
         } else {
-            this.setState({error:'Geolocation is not supported'})
-            /* geolocation IS NOT available */
+            this.setState({ error: 'Geolocation is not supported.'});
         }
     }
+
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
-        const {latitude, longitude} = position.coords;
-        localStorage.setItem(POS_KEY,JSON.stringify({lat: latitude, lon: longitude}));
-        this.setState({isLoadingGeoLocation: false});
-        this.loadNearbyPost();
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longitude }));
+        this.setState({ isLoadingGeoLocation: false });
+        this.loadNearbyPosts();
     }
 
     onFailedLoadGeoLocation = () => {
-        this.setState({isLoadingGeoLocation: false, error:'Geolocation is not supported'});
+        this.setState({ isLoadingGeoLocation: false, error: 'Failed to load geolocation.' });
     }
 
-    loadNearbyPost = () => {
+    loadNearbyPosts = () => {
         const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
         const token = localStorage.getItem(TOKEN_KEY);
-        this.setState({ isLoadingPosts: true, error: ''})
-        fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`, {
+        this.setState({ isLoadingPosts: true, error: '' });
+        return fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`, {
             method: 'GET',
             headers: {
                 Authorization: `${AUTH_HEADER} ${token}`,
@@ -57,21 +54,22 @@ export class Home extends React.Component{
             throw new Error('Failed to load posts.');
         }).then((data) => {
             console.log(data);
-            this.setState({ isLoadingPosts: false, posts: data? data : []})
+            this.setState({ isLoadingPosts: false, posts: data ? data : [] });
         }).catch((e) => {
             console.log(e.message);
-            this.setState({ isLoadingPosts: false})
+            this.setState({ isLoadingPosts: false, error: e.message });
         });
     }
+
     getImagePosts = () => {
-        const {error, isLoadingGeoLocation, isLoadingPosts, posts} = this.state;
-        if (error){
+        const { error, isLoadingGeoLocation, isLoadingPosts, posts } = this.state;
+        if (error) {
             return <div>{error}</div>
-        }else if (isLoadingGeoLocation){
-            return <Spin tip='Loading geo location...'/>
-        }else if (isLoadingPosts){
-            return <Spin tip='Loading posts...'/>
-        }else if (posts.length > 0){
+        } else if(isLoadingGeoLocation) {
+            return <Spin tip="Loading geo location..."/>
+        } else if (isLoadingPosts) {
+            return <Spin tip="Loading posts..." />
+        } else if (posts.length > 0) {
             const images = this.state.posts.map((post) => {
                 return {
                     user: post.user,
@@ -81,24 +79,24 @@ export class Home extends React.Component{
                     thumbnailWidth: 400,
                     thumbnailHeight: 300,
                 }
-            })
-            return (<Gallery images={images}/>)
-        }
-        else{
-            return 'Content of tab 1'
+            });
+
+            return (<Gallery images={images}/>);
+        } else {
+            return 'No nearby posts.';
         }
     }
-    render(){
-        const operations = <Button type="primary"> Create New Post </Button>
-        return(
+
+    render() {
+        const operations = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
+        return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Image Posts" key="1">
-                    {/*什么时候加（），什么时候不加？*/}
                     {this.getImagePosts()}
                 </TabPane>
                 <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
                 <TabPane tab="Map" key="3">Content of tab 3</TabPane>
             </Tabs>
-        )
+        );
     }
 }
--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@ export const GEO_OPTIONS = {
     maximumAge        : 3600000,
     timeout           : 27000
 };
-export const POS_KEY = 'POS_KEY'
-export const AUTH_HEADER = 'Bearer'
+export const POS_KEY = 'POS_KEY';
+export const AUTH_HEADER = 'Bearer';
